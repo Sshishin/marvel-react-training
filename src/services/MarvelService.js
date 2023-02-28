ee29def6125679e1fa71d74f5056b7b46f2ce441
Sergey Shishin <flyers32rus@gmail.com>
2023-02-28 22:28:46 +0300

    improvement: cleand and optimized sturcture

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,22 +11,23 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=bd5afe896d3938eaf9720dae3807fba6')
+    getAllCharacters = async () => {
+        const res = await this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=bd5afe896d3938eaf9720dae3807fba6')
+        return res.data.results.map(this._transformerCharacters);
     }
 
     getCharacter = async (id) => {
         const res = await this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=bd5afe896d3938eaf9720dae3807fba6`)
-        return this._transformerCharacters(res)
+        return this._transformerCharacters(res.data.results[0]);
     }
 
-    _transformerCharacters = (res) => {
+    _transformerCharacters = (char) => {
         return {
-            name: res.data.results[0].name,
-            description: res.data.results[0].description,
-            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
-            homepage: res.data.results[0].urls[0].url,
-            wiki: res.data.results[0].urls[1].url
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
         }
     }
 }
