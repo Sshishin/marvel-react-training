cff9a3d3a92d2379387de440bdb134b790a98875
Sergey Shishin <flyers32rus@gmail.com>
2023-02-28 22:11:25 +0300

    feat: transform data

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -15,8 +15,19 @@ class MarvelService {
         return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=bd5afe896d3938eaf9720dae3807fba6')
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=bd5afe896d3938eaf9720dae3807fba6`)
+    getCharacter = async (id) => {
+        const res = await this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=bd5afe896d3938eaf9720dae3807fba6`)
+        return this._transformerCharacters(res)
+    }
+
+    _transformerCharacters = (res) => {
+        return {
+            name: res.data.results[0].name,
+            description: res.data.results[0].description,
+            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+            homepage: res.data.results[0].urls[0].url,
+            wiki: res.data.results[0].urls[1].url
+        }
     }
 }
 
